153e4e7c66f547118b2b1a12a6f00d6d6a4fc49a
FrancoRuiz04 <fb096581@gmail.com>
2026-04-11 13:03:49 -0300

    scd commit

--- proyecto/src/main/java/ar/edu/unlar/programacion3/proyecto/objects/Estudiante.java
+++ proyecto/src/main/java/ar/edu/unlar/programacion3/proyecto/objects/Estudiante.java
@@ -1,5 +1,9 @@
 package ar.edu.unlar.programacion3.proyecto.objects;
 
 public class Estudiante {
+    private String legajo;
+    private String nombre;
+    private String carrera;
+    private String email;
 
 }
--- proyecto/src/main/java/ar/edu/unlar/programacion3/proyecto/objects/Libro.java
+++ proyecto/src/main/java/ar/edu/unlar/programacion3/proyecto/objects/Libro.java
@@ -1,5 +1,9 @@
 package ar.edu.unlar.programacion3.proyecto.objects;
 
 public class Libro {
-
+    private String ISBN;
+    private String titulo;
+    private String autor;
+    private int anio;
+    private boolean disponible;
 }
